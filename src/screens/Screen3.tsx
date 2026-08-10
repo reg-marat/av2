bfbf8b442d3f954b6904cc5a97ad7b99a62fec91
dantez13 <reg-marat@tut.by>
2026-08-10 17:32:40 +0700

    Update Screen3.tsx

--- src/screens/Screen3.tsx
+++ src/screens/Screen3.tsx
@@ -83,7 +83,7 @@ export default function Screen3({ onPasswordSuccess, onModalClose }: Screen3Prop
           <p className="text-center text-white/70 text-xs font-light">
             Se hai qualche domanda -{' '}
             <a
-              href="https://t.me/DanteValdesBot"
+              href="https://t.me/DanteValdes_Bot"
               target="_blank"
               rel="noopener noreferrer"
               className="text-yellow-400 hover:text-yellow-500 transition-colors"
