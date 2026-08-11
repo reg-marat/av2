1cf70e83a8716c87fc305ad4ee6e48910b99a8c8
dantez13 <reg-marat@tut.by>
2026-08-11 14:06:26 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Screen1 from './screens/Screen1';
 import Screen2 from './screens/Screen2';
 import Screen3 from './screens/Screen3';
 import Screen4 from './screens/Screen4';
-import ScreenNoReg from './screens/ScreenNoRegs';
+import ScreenNoReg from './screens/ScreenNoReg';
 import ScreenNoDep from './screens/ScreenNoDep';
 import ScreenLux from './screens/ScreenLux';
 
